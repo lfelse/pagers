3372bf41e0a9cf6a8549176ba757d1fbb8a0f1c2
ad <mm@itmp.top>
2016-06-05 22:35:04 +0800

    fix a bug

--- app/src/main/java/io/lf/pagers/MainActivity.java
+++ app/src/main/java/io/lf/pagers/MainActivity.java
@@ -124,6 +124,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         viewPager.removeOnPageChangeListener(onPageChangeListener);
